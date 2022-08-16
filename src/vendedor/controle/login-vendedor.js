ee280bfff3a5ae042f0e19f67df3c95d607b8a87
gustavo-gabriel-OL <gustavogbriel2004@gmail.com>
2022-08-16 12:09:20 -0300

    16.08

---
+++ src/vendedor/controle/login-vendedor.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    $('.btn-login').click(function(e) {
+
+        e.preventDefault();
+
+        let dados = $('#form-login').serialize()
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            assync: true,
+            data: dados,
+            url: 'src/vendedor/modelo/login-vendedor.php',
+            success: function(dados) {
+
+                if(dados.tipo == 'success'){
+                    $(location).attr('href', 'sistema.html')
+                }else{
+                Swal.fire({
+                    title: 'Rifadástico',
+                    text: dados.mensagem,
+                    icon: dados.tipo,
+                    confirmButtonText: 'OK'
+                })
+
+              }
+            }
+        })
+    })
+
+})
